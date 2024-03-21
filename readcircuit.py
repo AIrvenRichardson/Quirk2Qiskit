85863da98841c4d9197cd5d520325a4e5e17a66c
AIrvenRichardson <airvenrichardson@aol.com>
2024-03-21 12:15:09 -0600

    No longer uses selenium, regex is way faster

--- readcircuit.py
+++ readcircuit.py
@@ -1,26 +1,26 @@
-#Quirk stores circuits as json in the page element, so we will use selenium to grab the url's html
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-#tkinter is weirdly useful for reading the clipboard.
-import tkinter as tk
-
+#Quirk stores circuits in the url, so I simply have to parse the string
+#Heres a test link https://algassert.com/quirk#circuit={%22cols%22:[[%22H%22],[%22%E2%80%A2%22,%22X%22]]}
+#It is the bell plus state
+import re #gonna need this for parsing.
 
 print('Hand over the link:')
-x = input()
+url = "https://algassert.com/quirk#circuit={%22cols%22:[[%22H%22],[%22%E2%80%A2%22,%22X%22]]}" #use input(), hardcoded is for testing quickly
 print("thanks") 
 
+#https://www.geeksforgeeks.org/python-extract-substrings-between-brackets/
+regsult = re.findall(r'\{.*?\}', url)
+circuitString = regsult[0]
+circuitString = circuitString.replace("%22", "")
+ees = re.findall(r'\[.*?\]', circuitString)
 
-#Get webdriver and then open the link (that will be given by the user)
-driver = webdriver.Firefox()
-driver.get(x)
+#Now grab each gate for each circuit, make a 2d array of [Qubit][Gate]
+qubitOps = []
+for wire in ees:
+    line = []
+    operations = wire.split(',')
+    for gate in operations:
+        pureGate = gate.replace("[", "").replace("]", "")
+        line.append(pureGate)
+    qubitOps.append(line)
 
-#Click the export button so you can then click the copy button
-expbutton = driver.find_element(value="export-button")
-expbutton.click()
-copybutton = driver.find_element(value="export-json-copy-button")
-copybutton.click()
-root = tk.Tk()
-root.withdraw()  # to hide the window
-variable = root.clipboard_get()
-print(variable)
+print(qubitOps)
